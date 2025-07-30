4f9e397cf8da193d3de40d0d45b63b255b5aa808
JJ <josejromero24@gmail.com>
2025-07-30 09:05:18 +0200

    Added search bar

--- DragonBallVIPER/View/CharacterScrollView.swift
+++ DragonBallVIPER/View/CharacterScrollView.swift
@@ -8,16 +8,35 @@ import SwiftUI
 
 struct CharacterListView: View {
     @ObservedObject var presenter: CharactersPresenter
+    @State private var searchText: String = ""
+
+    var filteredCharacters: [Item] {
+        if searchText.isEmpty {
+            return presenter.characters
+        } else {
+            return presenter.characters.filter {
+                $0.name.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
 
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVStack(spacing: 16) {
-                    ForEach(presenter.characters, id: \.id) { character in
-                        CharacterRowView(character: character)
+            VStack {
+                TextField("Buscar por nombre...", text: $searchText)
+                    .padding(10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .padding([.horizontal, .top])
+
+                ScrollView {
+                    LazyVStack(spacing: 16) {
+                        ForEach(filteredCharacters, id: \.id) { character in
+                            CharacterRowView(character: character)
+                        }
                     }
+                    .padding()
                 }
-                .padding()
             }
             .navigationTitle("Characters")
             .onAppear {
